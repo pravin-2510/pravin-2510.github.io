c4adb1507831032a16628020b68914e85b0678b8
pravin-2510 <67089248+pravin-2510@users.noreply.github.com>
2020-06-26 19:00:25 +0530

    Update script.js

--- module_4_solution/script.js
+++ module_4_solution/script.js
@@ -3,21 +3,28 @@ Solution of assignment 4:
 
 Expected output:
 
-Hello pravin
-Good Bye Jay
-Hello Sanjana
-Hello raj
-Good Bye Jagdish
-Hello akash
-Hello harshal
-Good Bye jayshri
-Hello Pratik
-Hello Asmita
-Good Bye Janaki
+/*
+Hello Yaakov
+Good Bye John
+Good Bye Jen
+Good Bye Jason
+Hello Paul
+Hello Frank
+Hello Larry
+Hello Paula
+Hello Laura
+Good Bye Jim
+WARNING!!! WARNING!!!
+The code does NOT currently work! It is YOUR job to make it work
+as described in the requirements and the steps in order to complete this
+assignment.
+WARNING!!! WARNING!!!
+*/
 */
 
 (function() {
-    var names = ["pravin", "jay","Sanjana", "raj", "jagdish", "akash", "harshal", "jayshri", "pratik", "Asmta", "janaki"];
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
     for (var i = 0; i < names.length; i++) {
         var firstLetter = names[i].charAt(0).toLowerCase();
         if (firstLetter === 'j') {
